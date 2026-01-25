d894b9359cdd46fa47aa0675495d17470f937667
Ruxshona <mamatisaqovaruhshona@gmail.com>
2026-01-25 11:31:52 +0500

    yangilandi

--- Crm_loyiha/src/App.jsx
+++ Crm_loyiha/src/App.jsx
@@ -1,11 +1,15 @@
 import Nav from "./navbar/navbar";
 import './App.css'
+import {Routes,Route} from "react-router-dom";
 
 function App() {
 
   return (
     <>
+    <Routes>
      <Nav/>
+    <Route/>
+    </Routes>
     </>
   )
 }
--- Crm_loyiha/src/navbar/navbar.jsx
+++ Crm_loyiha/src/navbar/navbar.jsx
@@ -1,7 +1,8 @@
+import "./navbar.scss";
 function Nav() {
     return(
         <div className="nav">
-            <h1>navbar</h1>
+            <h1><img src="./img/image.png" alt="" />Dashboard</h1>
         </div>
     )
 }
